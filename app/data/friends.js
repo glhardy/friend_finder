23f17c3af6c93b15bcc7d21605037c625d3aa8b2
Gabrielle Hardy <gabrhardy@gmail.com>
2017-08-27 17:30:26 -0500

    idk

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = []
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
