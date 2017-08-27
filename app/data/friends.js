c23a8726fa44833f3b9618b88c7e6518709bbcef
Gabrielle Hardy <gabrhardy@gmail.com>
2017-08-27 18:24:06 -0500

    meow

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,9 +4,71 @@
 // ===============================================================================
 
 
-var friends = []
-  }
-
+var friends = [
+  {
+    name: "Pikachu",
+    photo: "http://www.pelucy.com.br/image/cache/data/produtos/pokemon/pikachu2-500x500.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Bulbasaur",
+    photo: "http://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/500px-001Bulbasaur.png",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Charmander",
+    photo: "https://vignette4.wikia.nocookie.net/pokemontowerdefensetwo/images/2/2e/Charmander_banner.jpg/revision/latest?cb=20160623043900",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Squirtle",
+    photo: "http://pokemonqrcode.com/image/cache/catalog/Pokemon/Gen1/squirtle-500x500.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
 ];
 
 // Here we export the array. This makes it accessible to other files using require.
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,6 +16,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
+  app.get("/css", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../css/style.css"));
+  });
+
   // If no matching route is found default to home
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
